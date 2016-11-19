ea3b1315ccda3747d14d70803234ce14fcaffcca
makushev.a <a.s.makushev@gmail.com>
2016-11-19 17:32:38 +0300

    Add API Concrollers
    добавлены Concroller`ы отвечающие за API

---
+++ MyBlog/src/WebView/API/RegistrationLogicController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelCore;
+using Microsoft.AspNetCore.Mvc;
+using WebView.Web.Controllers;
+
+namespace WebView.API
+{
+    public class RegistrationLogicController
+    {
+        IRegistrationLogic model;
+        IAuthorization auth;
+
+
+        public RegistrationLogicController(IRegistrationLogic registrationLogic, IAuthorization authContext)
+        {
+            model = registrationLogic;
+            auth = authContext;
+        }
+
+
+        //надо подумать как лучше сделать
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string userName)
+        {
+            bool result = model.ProverkaSvobodenliUserName(userName);
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string userEmail)
+        {
+            bool result = model.ProverkaSvobodenliEmail(userEmail);
+
+            return new JsonResult(result);
+        }
+        
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]string name, [FromBody]string email, [FromBody]string password)
+        {
+            bool result = model.PreRegistration(name, email, password);
+
+            return new JsonResult(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]string code)
+        {
+            bool result = await model.EndRegistration(code);
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ MyBlog/src/WebView/API/UserProfileController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebView.Web.Controllers;
+
+namespace WebView.API
+{
+    public class UserProfileControllerAPI
+    {
+        IUserProfileLogic model;
+        IAuthorization auth;
+
+
+        public UserProfileControllerAPI(IUserProfileLogic userProfileLogic, IAuthorization authContext)
+        {
+            model = userProfileLogic;
+            auth = authContext;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string userId)
+        {
+            User user = model.GetUserProfile(userId);
+
+            //if(coments==null)
+            // return   Error
+
+            return new JsonResult(user);
+        }
+
+
+        //PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] User user)
+        {
+            int? id = await model.ChangeUserProfile(user);
+            //if(id==null)
+            // return   Error
+            return new JsonResult(id);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string userId)
+        {
+            User user = model.GetUserProfile(userId);
+            user.State.StateElement = StateEnum.Removed;
+
+            return new JsonResult(await model.ChangeUserProfile(user));
+        }
+    }
+
+
+       
+    
+}
